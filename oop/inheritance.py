67a59ab736e8d98ddc7b805ff700ce4ff8a9f6f2
shaik Mujahed <shaikmujahed002@gmail.com>
2020-02-29 01:47:50 +0530

    A child class inherit to parent class class methode and attributes.

--- oop/inheritance.py
+++ oop/inheritance.py
@@ -1,1 +1,21 @@
+class Decimal:
+    def __init__(self,number,place):
+        self.number = number
+        self.place = place
+    def __repr__(self):
+        return "%.{}f".format(self.place) % self.number
+
+
+class Currency(Decimal):
+    def __init__(self,symbol,number,place):
+        super().__init__(number, place)
+        self.symbol = symbol
+
+    def __repr__(self):
+        return "{}{}".format(self.symbol,super().__repr__())
+
+
+print(Decimal(11.3256,2))
+print(Currency("$",15.4565,3))
+
 
